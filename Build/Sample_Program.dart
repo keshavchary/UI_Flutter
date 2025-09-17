62d07993a5fb5339045cb961d8a647d50c273575
keshavchary <kasanikeshavachary@gmail.com>
2025-09-17 15:56:29 +0530

    Add files via upload

---
+++ Build/Sample_Program.dart
@@ -0,0 +1,16 @@
+void main() {
+  // Name
+  var name = "keshi";
+  print("Name: $name");
+
+  // Skills
+  var skills = <String>["c", "c++", "Dart"];
+  for (var i = 0; i < 1; i++) {
+    print("First Skill: ${skills[i]}");
+  }
+
+  // Sum
+  int a = 30, b = 50;
+  int result = a + b;
+  print("Sum: $result");
+}
